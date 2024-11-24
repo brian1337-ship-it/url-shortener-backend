a414967b97b284cac3a6dc1373990bc93534d96d
Brian <rollinglights90@gmail.com>
2024-11-24 09:24:41 +0400

    refactored server initialization

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express, { type Express } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import helmet from "helmet";
 import logger from "@/utils/logger";
 import { env } from "@/utils/envConfig";
 
 const app: Express = express();
-const { NODE_ENV, HOST, PORT } = env;
+
+const httpServer = createServer(app);
 
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
@@ -16,31 +19,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
 
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: env.CORS_ORIGIN,
+//     credentials: true,
+//   },
+// });
+
 // Routes
-app.get("/", (_, res) => res.send(`Server is up and running version ${HOST}`));
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${env.HOST}`)
+);
 
-// the server instance
-const server = app.listen(PORT, async () => {
-  logger.info(`Server (${NODE_ENV}) listening  on port http://${HOST}:${PORT}`);
-});
-
-// signals to listen to for termination
-const signals = ["SIGTERM", "SIGINT"];
-
-const gracefulShutdown = (signal: string) => {
-  process.on(signal, async () => {
-    logger.info("Got signal", signal);
-    server.close();
-
-    logger.info("Server closed");
-
-    process.exit(0);
-  });
-};
-
-// if signal detected execute graceful shutdown
-for (let i = 0; i < signals.length; i++) {
-  gracefulShutdown(signals[i]);
-}
-
-// export { app, logger };
+export { app, logger };
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { env } from "@/utils/envConfig";
+import { app, logger } from "@/app";
+
+const { NODE_ENV, HOST, PORT } = env;
+
+// the server instance
+const server = app.listen(PORT, async () => {
+  logger.info(`Server (${NODE_ENV}) listening  on port http://${HOST}:${PORT}`);
+
+  // initialize the socket.io
+  // socket({ io });
+});
+
+// signals to listen to for termination
+const signals = ["SIGTERM", "SIGINT"];
+
+const gracefulShutdown = (signal: string) => {
+  process.on(signal, async () => {
+    logger.info("Got signal", signal);
+    httpServer.close();
+
+    logger.info("Server closed");
+
+    process.exit(0);
+  });
+};
+
+// if signal detected execute graceful shutdown
+for (let i = 0; i < signals.length; i++) {
+  gracefulShutdown(signals[i]);
+}
